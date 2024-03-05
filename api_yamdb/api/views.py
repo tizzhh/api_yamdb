1548353dcd52d244ceffbbd3dea760480f8106c6
Vadim Kuznetsov <darovadraste@gmail.com>
2024-03-05 15:32:58 +0300

    Add email service (#2)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,7 @@
+from random import randint
+
+from django.core.exceptions import BadRequest
+from django.core.mail import send_mail
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
@@ -10,10 +14,27 @@ class UserViewSetAuth(viewsets.ModelViewSet):
     serializer_class = UserSerializerAuth
 
     def create(self, request, *args, **kwargs):
+        user = CustomUser.objects.filter(username=request.data.get('username'))
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
+        if not user:
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+
+        if user and user[0].email != request.data.get('email'):
+            raise BadRequest('Incorrect email')
+        self.send_confirmation_code_email(request.data)
+
+        headers = self.get_success_headers(serializer.initial_data)
+
         return Response(
-            serializer.data, status=status.HTTP_200_OK, headers=headers
+            serializer.initial_data, status=status.HTTP_200_OK, headers=headers
+        )
+
+    def send_confirmation_code_email(self, data):
+        send_mail(
+            subject='Confirmation code',
+            message=f'Dear {data.get("username")}, here\'s your confirmation code: {randint(1000, 9999)}',
+            from_email='yamdb@yamdb.net',
+            recipient_list=(data.get('email'),),
+            fail_silently=True,
         )
