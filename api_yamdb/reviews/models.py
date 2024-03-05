c3693bee510bab8bca45ba835a1da707b8172412
Диана Кужамбетова <dkuzhambetova@yandex.ru>
2024-03-06 01:54:03 +0300

    Create review model and serializer.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,45 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    """Model describes parameters of review on title."""
+
+    text = models.TextField('Текст отзыва')
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
+    score = models.PositiveSmallIntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(1, 'Оценка не может быть ниже 1.'),
+            MaxValueValidator(10, 'Оценка не может быть выше 10.')
+        ],
+        verbose_name='Оценка'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации отзыва'
+    )
+
+    class Meta:
+        default_related_name = 'reviews'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_title'
+            )
+        ]
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
