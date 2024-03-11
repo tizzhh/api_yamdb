ab5bad2d254b6ea7bfb4553bea0ad2db6b375dcc
Vadim Kuznetsov <darovadraste@gmail.com>
2024-03-11 22:52:22 +0300

    Add user model to admin

--- api_yamdb/yamdb_user/admin.py
+++ api_yamdb/yamdb_user/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import YamdbUser
+
+
+class YamdbUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role',
+    )
+    search_fields = ('username', 'email', 'role')
+    list_filter = 'role'
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(YamdbUser)
