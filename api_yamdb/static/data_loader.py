0846c0203b301a2a7243800243637bd20dd62ada
Kuznetsov Vadim <darovadraste@gmail.com>
2024-03-12 13:48:42 +0300

    Add db loading script

---
+++ api_yamdb/static/data_loader.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+import pandas as pd
+
+TABLE_DATA = {
+    'data/category.csv': 'reviews_category',
+    'data/comments.csv': 'reviews_comment',
+    'data/genre_title.csv': 'reviews_title_genre',
+    'data/review.csv': 'reviews_review',
+    'data/titles.csv': 'reviews_title',
+    'data/users.csv': 'yamdb_user_yamdbuser',
+}
+
+with sqlite3.connect('../db.sqlite3') as conn:
+    for csv_path, table_name in TABLE_DATA.items():
+        df = pd.read_csv(csv_path)
+        df.columns = df.columns.str.strip()
+        df.to_sql(table_name, conn, if_exists='replace')
